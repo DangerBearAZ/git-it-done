96c283ce7a2b50e0696c1a5e3ab7000bc9aeb2e9
claire ogesen <claire.ogesen.co@gmail.com>
2021-03-03 07:28:15 -0700

    searchTerm is not defined- not working

--- assets/css/homepage.js
+++ assets/css/homepage.js
@@ -1,5 +1,11 @@
 
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-serch-term");
+
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -8,9 +14,28 @@ var getUserRepos = function(user) {
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
         console.log(data);
+        displayRepos(data, user); 
       });
     });
   };
 
-  getUserRepos("lernantino");
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //get value from input element 
+    var username = nameInputEl.value.trim();
+
+    if (username){
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos, serchTerm) {
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
